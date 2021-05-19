b00b119a0ca074048c7d8d93379111f161ea3b8c
SvanBoxel <SvanBoxel@users.noreply.github.com>
2021-05-19 08:49:12 +0000

    deploy: 1a465ad9d910ef31a4daa9c057d0788436e5f363

---
+++ _nuxt/static/1621414148/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fresources","\u002Fcollections","\u002Fcollection\u002Fcodespaces","\u002Fcollection\u002Fgithub-advanced-security","\u002Fcollection\u002Fgithub-pages"]})
